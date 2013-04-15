fd63bfd4090d2edf006458e1380bbd84e32a36a5
Tyler Stromberg <tylers@mac.com>
2013-04-15 18:09:41 +0200

    Basic address book notification handling

--- Source/GitAddressBook/Classes/L42AppDelegate.m
+++ Source/GitAddressBook/Classes/L42AppDelegate.m
@@ -7,11 +7,45 @@
 
 #import "L42AppDelegate.h"
 
+#import <AddressBook/AddressBook.h>
+
 @implementation L42AppDelegate
+{
+@private
+    ABAddressBook *_addressBook;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    // Listen for changes to the address book
+    _addressBook = [ABAddressBook sharedAddressBook];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(addressBookChanged:)
+                                                 name:kABDatabaseChangedExternallyNotification
+                                               object:nil];
+}
+
+- (void)addressBookChanged:(NSNotification *)notification
+{
+    NSLog(@"Address book changed:");
+    
+    if (notification.userInfo[kABInsertedRecords] == nil &&
+        notification.userInfo[kABUpdatedRecords] == nil &&
+        notification.userInfo[kABDeletedRecords] == nil)
+    {
+        NSLog(@"Everything changed.");
+    }
+    else
+    {
+        NSArray *insertedRecords = notification.userInfo[kABInsertedRecords];
+        NSArray *updatedRecords = notification.userInfo[kABUpdatedRecords];
+        NSArray *deletedRecords = notification.userInfo[kABDeletedRecords];
+        
+        NSLog(@"  Inserted: %@", insertedRecords ?: @"0");
+        NSLog(@"  Updated: %@", updatedRecords ?: @"0");
+        NSLog(@"  Deleted: %@", deletedRecords ?: @"0");
+    }
 }
 
 @end
